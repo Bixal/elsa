3ad934b9b9009a57a46fdc8af8a7cd7d040c93d7
$cott <scott.queen@bixal.com>
2018-01-19 14:33:07 -0500

    removing old theme files

--- html/themes/custom/elsa/components/03-molecules/search-form/block--search-form.js
+++
@@ -1,3 +0,0 @@
-$('#submit').on('click', function(){
-  alert("Use the power of the SHWARTZ!");
-});
--- html/themes/custom/elsa/js/elsa.js
+++
@@ -1,4 +0,0 @@
-(function ($) {$('#submit').on('click', function(){
-  alert("Use the power of the SHWARTZ!");
-});
-}(jQuery));
